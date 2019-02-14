101004c0ff9f57691652d184db39ac8104c726a1
Denise Lashlley <htmlash@gmail.com>
2019-02-14 11:13:05 +0000

    add navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,59 +1,90 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./styles.scss";
 import CustomLink from "../CustomLink";
 
-export const NavbarTemplate = ({ data }) => (
-  <nav className="navbar">
-    
-    <div className="container navbar-container">
-      <div className="site-brand">
-        <a href="/" className="site-logo">
-          <span>The Light Tree</span>
-        </a>
-      </div>
-      {data.menuItems.length > 0 && (
-        <ul className="navbar-menu">
-          {data.menuItems.map(menuItem => (
-            <li key={menuItem.linkURL} className="navbar-menuItem">
-              <CustomLink
-                linkType={menuItem.linkType}
-                linkURL={menuItem.linkURL}
-                className="navbar-menuLink"
-              >
-                {menuItem.label}
-              </CustomLink>
-            </li>
-          ))}
-        </ul>
-      )}
-      { data.socialLinks.length > 0 && (
-        <ul className="social-icons">
-          {data.socialLinks.map(socialitems => (
-            <li key={socialitems.linkURL} className="social-icons--item">
-              <a 
-                href={socialitems.linkURL}
-                aria-label={socialitems.label}
-                className="icon-link"
-              >
-                <i className={`icon icon-${socialitems.type} `} />
-                <span>{socialitems.hiddenText}</span>
-              </a>
-            </li>
-          ))}
-        </ul>
-        )
-      }
-    </div>
-  </nav>
-);
-
-const Navbar = props => {
-  if (!props.data) {
-    return null;
+export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuState: false
+    }
   }
-  const data = props.data.edges[0].node.frontmatter;
-  return <NavbarTemplate data={data} />;
-};
 
-export { Navbar };
+  handleShowMenu(event) {
+    event.preventDefault();
+    this.setState({
+      menuState: this.state.menuState === false,
+    });
+  }
+
+  render() {
+    const data = this.props.data.edges[0].node.frontmatter;
+
+    const menuClass = this.state.menuState ? 'open' : 'close';
+    const activeState = menuClass === 'open' ? 'active' : 'inactive';
+
+    return (
+      <header className="site-header">  
+        <div className="container navbar-container">
+      <nav className="navbar">
+        <div className="site-brand">
+        <CustomLink 
+          linkURL="/" 
+          linkType="internal" 
+          className="site-logo"
+        >
+          <span>The Light Tree</span>
+        </CustomLink>
+        </div>
+          <a 
+          href="/" 
+          className={`navbar-btn ${activeState} `}
+          onClick={e => this.handleShowMenu(e)}
+        >
+          <span/>
+        </a>
+        <div className={`navbar-wrapper ${menuClass}`}>
+          {data.menuItems.length > 0 && (
+            <ul className="navbar-menu">
+              {data.menuItems.map(menuItem => (
+                <li key={menuItem.linkURL} className="navbar-menuItem">
+                  <CustomLink
+                    linkType={menuItem.linkType}
+                    linkURL={menuItem.linkURL}
+                    className="navbar-menuLink"
+                  >
+                    {menuItem.label}
+                  </CustomLink>
+                </li>
+              ))}
+            </ul>
+          )}
+          { data.socialLinks.length > 0 && (
+            <ul className="social-icons">
+              {data.socialLinks.map(socialitems => (
+                <li key={socialitems.linkURL} className="social-icons--item">
+                  <a 
+                    href={socialitems.linkURL}
+                    aria-label={socialitems.label}
+                    className="icon-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className={`icon icon-${socialitems.type} `} />
+                    <span>{socialitems.hiddenText}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+            )
+          }
+        </div>
+      </nav>
+    </div>
+      </header>
+    );
+  }
+
+}
+
