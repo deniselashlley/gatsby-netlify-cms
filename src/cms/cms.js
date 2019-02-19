d059843af74a59cc9fd7cb1c3dfbe177471af9a4
Denise Lashlley <htmlash@gmail.com>
2019-02-19 01:47:04 +0000

    remove redudant files

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,16 +4,13 @@ import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import ClassesPagePreview from "./preview-templates/ClassesPagePreview";
 import TeachingPagePreview from "./preview-templates/TeachingPagePreview";
 import HomePagePreview from "./preview-templates/HomePagePreview";
-import MeetupPreview from "./preview-templates/MeetupPreview";
 import FooterPreview from "./preview-templates/FooterPreview";
 import NavbarPreview from "./preview-templates/NavbarPreview";
 import PastMeetupsPagePreview from "./preview-templates/PastMeetupsPagePreview";
 
-CMS.registerPreviewTemplate("meetups", MeetupPreview);
 CMS.registerPreviewTemplate("footer", FooterPreview);
 CMS.registerPreviewTemplate("navbar", NavbarPreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("classes", ClassesPagePreview);
 CMS.registerPreviewTemplate("teaching", TeachingPagePreview);
 CMS.registerPreviewTemplate("home", HomePagePreview);
-CMS.registerPreviewTemplate("pastMeetups", PastMeetupsPagePreview);
