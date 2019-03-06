5244e1426733cb513b0d5a5818a24bdbf39043c8
Denise Lashlley <htmlash@gmail.com>
2019-03-06 23:59:24 +0000

    update ssr

--- gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-exports.onRenderBody = ({ setPreBodyComponents }) => {
-    setPreBodyComponents(<div 
-        id="iframe-calendar"
-        data-tockify-component="calendar" 
-        data-tockify-calendar="denise.lashlley" 
-        className="embed-calender"
-      />)
-}
